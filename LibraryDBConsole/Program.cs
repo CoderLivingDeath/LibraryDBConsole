36aa852a2ab0453e4ef21cb20e319ab47aae56b6
CoderLivingDeath <CoderLivingDeath@gmail.com>
2024-12-22 07:37:06 +0700

    ну типо да

--- LibraryDBConsole/Program.cs
+++ LibraryDBConsole/Program.cs
@@ -13,7 +13,15 @@ namespace LibraryDBConsole
         static void Main(string[] args)
         {
             MainView view = new MainView();
-            App app = new App("server=127.0.0.1;uid = root; pwd=26569; database=library");
+            App app = new App("server=127.0.0.1;database=sys");
+
+            Console.WriteLine("=== Команда help для отображение списко команд.");
+            Console.WriteLine("=== Используйте команду SetConnection [connection string] для формирования корректного соединения с базой данных MySQL.");
+            Console.WriteLine("=== Используй команду ChangeBD [database name] для изменения базы данных.");
+            Console.WriteLine("=== Для отображение всех баз данных используйте команду GetAllDbnames.");
+            Console.WriteLine("=== Для открытия и закрытия соединения используйте команду OpenConnection и CloseConnection.");
+            Console.WriteLine("=== Для отображение логов используйте команду Debug.");
+            Console.WriteLine("=== Для отображение статуса текущего соединение используйте команду Status.");
 
             view.Initialize(app, true);
         }
